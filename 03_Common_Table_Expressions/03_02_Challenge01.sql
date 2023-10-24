380ce2fb93887f5ee0cc1ee41e2302f707c841d3
Kedeisha <93458473+kedeisha1@users.noreply.github.com>
2023-10-24 02:58:01 -0400

    Create 03_02_Challenge01.sql

---
+++ 03_Common_Table_Expressions/03_02_Challenge01.sql
@@ -0,0 +1,3 @@
+-- Calculate the total sales amount (based on product price and quantity sold) for each month in the year 2022.
+-- The result should have two columns: Month (formatted as YYYY-MM) and TotalSalesAmount
+-- Order the result by month in ascending order.
